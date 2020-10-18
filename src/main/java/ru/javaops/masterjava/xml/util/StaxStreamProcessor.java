8fda1c15e129e2ac8efbdfe7331c70661561b8b8
ipodskochin <ipodskochin@mail.ru>
2020-10-18 23:06:38 +0300

    HW02_Optional(part01): MainXml implemented with StAX

--- src/main/java/ru/javaops/masterjava/xml/util/StaxStreamProcessor.java
+++ src/main/java/ru/javaops/masterjava/xml/util/StaxStreamProcessor.java
@@ -1,18 +1,32 @@
 package ru.javaops.masterjava.xml.util;
 
+import ru.javaops.masterjava.xml.schema.User;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StaxStreamProcessor implements AutoCloseable {
     private static final XMLInputFactory FACTORY = XMLInputFactory.newInstance();
 
-    private final XMLStreamReader reader;
+    private XMLStreamReader reader;
+    private XMLEventReader eventReader;
+    private InputStream inputStream;
 
     public StaxStreamProcessor(InputStream is) throws XMLStreamException {
+        inputStream = is;
         reader = FACTORY.createXMLStreamReader(is);
+        eventReader = FACTORY.createXMLEventReader(is);
     }
 
     public XMLStreamReader getReader() {
@@ -39,8 +53,75 @@ public class StaxStreamProcessor implements AutoCloseable {
         return doUntil(XMLEvent.START_ELEMENT, element) ? reader.getElementText() : null;
     }
 
+    public List<User> getUsersFromGroup(String groupId) throws XMLStreamException {
+        List<User> users = new ArrayList<>();
+        User user = null;
+        boolean isGroupIdMatched = false;
+
+        while (eventReader.hasNext()) {
+            XMLEvent nextEvent = eventReader.nextEvent();
+            if (nextEvent.isStartElement()) {
+                StartElement startElement = nextEvent.asStartElement();
+                switch (startElement.getName().getLocalPart()) {
+                    case "User":
+                        user = new User();
+                        isGroupIdMatched = false;
+                        Attribute city = startElement.getAttributeByName(new QName("city"));
+                        Attribute email = startElement.getAttributeByName(new QName("email"));
+                        user.setCity(city.getValue());
+                        user.setEmail(email.getValue());
+                        break;
+                    case "groups":
+                        nextEvent = eventReader.nextEvent();
+                        isGroupIdMatched = nextEvent.asCharacters().getData().contains(groupId);
+                        break;
+                    case "fullName":
+                        nextEvent = eventReader.nextEvent();
+                        user.setFullName(nextEvent.asCharacters().getData());
+                        break;
+                }
+            }
+
+            if (nextEvent.isEndElement()) {
+                EndElement endElement = nextEvent.asEndElement();
+                if (endElement.getName().getLocalPart().equals("User") && isGroupIdMatched) {
+                    users.add(user);
+                }
+            }
+        }
+        return users;
+    }
+
+    public String getGroupId(String groupName) throws XMLStreamException {
+        Attribute groupId = null;
+        while (eventReader.hasNext()) {
+            XMLEvent nextEvent = eventReader.nextEvent();
+            if (nextEvent.isStartElement()) {
+                StartElement startElement = nextEvent.asStartElement();
+                if (startElement.getName().getLocalPart().equals("Group")) {
+                    groupId = startElement.getAttributeByName(new QName("id"));
+                }
+                if (startElement.getName().getLocalPart().equals("name")) {
+                    nextEvent = eventReader.nextEvent();
+                    if (groupName.equals(nextEvent.asCharacters().getData())) {
+                        return groupId != null ? groupId.getValue() : "";
+                    }
+                }
+            }
+        }
+        return "";
+    }
+
     public String getText() throws XMLStreamException {
         return reader.getElementText();
+    }
+
+    public void reload(InputStream is) throws Exception {
+        inputStream.close();
+        reader.close();
+        eventReader.close();
+        reader = FACTORY.createXMLStreamReader(is);
+        eventReader = FACTORY.createXMLEventReader(is);
     }
 
     @Override
@@ -49,6 +130,22 @@ public class StaxStreamProcessor implements AutoCloseable {
             try {
                 reader.close();
             } catch (XMLStreamException e) {
+                // empty
+            }
+        }
+
+        if (eventReader != null) {
+            try {
+                eventReader.close();
+            } catch (XMLStreamException e) {
+                // empty
+            }
+        }
+
+        if (inputStream != null) {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
                 // empty
             }
         }
--- src/test/java/ru/javaops/masterjava/xml/MainXml.java
+++ src/test/java/ru/javaops/masterjava/xml/MainXml.java
@@ -1,38 +1,22 @@
 package ru.javaops.masterjava.xml;
 
 import com.google.common.io.Resources;
-import org.junit.Test;
-import ru.javaops.masterjava.xml.schema.ObjectFactory;
-import ru.javaops.masterjava.xml.schema.Payload;
-import ru.javaops.masterjava.xml.schema.Project;
 import ru.javaops.masterjava.xml.schema.User;
-import ru.javaops.masterjava.xml.util.JaxbParser;
-import ru.javaops.masterjava.xml.util.Schemas;
+import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class MainXml {
-    private static final JaxbParser JAXB_PARSER = new JaxbParser(ObjectFactory.class);
 
-    static {
-        JAXB_PARSER.setSchema(Schemas.ofClasspath("payload.xsd"));
-    }
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) {
-        Payload payload = null;
-
-        try {
-            payload = JAXB_PARSER.unmarshal(
-                    Resources.getResource("payload.xml").openStream());
-        } catch (Exception e) {
-            e.printStackTrace();
+        try (StaxStreamProcessor processor =
+                     new StaxStreamProcessor(Resources.getResource("payload.xml").openStream())) {
+            String groupId = processor.getGroupId(args[0]);
+            processor.reload(Resources.getResource("payload.xml").openStream());
+            List<User> users = processor.getUsersFromGroup(groupId);
+            users.forEach(u -> System.out.println(u.getFullName() + " " + u.getEmail()));
         }
-
-        List<String> usersOfProject = payload.getUsers().getUser().stream()
-                .filter(u -> u.getGroups().stream().anyMatch(g -> ((Project.Groups.Group) g).getName().equals(args[0])))
-                .map(u -> u.getFullName()).collect(Collectors.toList());
-
-        usersOfProject.forEach(System.out::println);
     }
 }
