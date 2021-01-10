2ff4bec91ce37e8f16cfa998ff031c08e501d82a
ipodskochin <ipodskochin@mail.ru>
2021-01-10 22:39:49 +0300

    liquidbase script + city/project/group model

--- persist/src/test/java/ru/javaops/masterjava/persist/UserTestData.java
+++ persist/src/test/java/ru/javaops/masterjava/persist/UserTestData.java
@@ -3,7 +3,7 @@ package ru.javaops.masterjava.persist;
 import com.google.common.collect.ImmutableList;
 import ru.javaops.masterjava.persist.dao.UserDao;
 import ru.javaops.masterjava.persist.model.User;
-import ru.javaops.masterjava.persist.model.UserFlag;
+import ru.javaops.masterjava.persist.model.type.UserFlag;
 
 import java.util.List;
 
@@ -17,12 +17,15 @@ public class UserTestData {
     public static List<User> FIST5_USERS;
 
     public static void init() {
-        ADMIN = new User("Admin", "admin@javaops.ru", UserFlag.superuser);
-        DELETED = new User("Deleted", "deleted@yandex.ru", UserFlag.deleted);
-        FULL_NAME = new User("Full Name", "gmail@gmail.com", UserFlag.active);
-        USER1 = new User("User1", "user1@gmail.com", UserFlag.active);
-        USER2 = new User("User2", "user2@yandex.ru", UserFlag.active);
-        USER3 = new User("User3", "user3@yandex.ru", UserFlag.active);
+        CityTestData.init();
+        CityTestData.setUp();
+
+        ADMIN = new User("Admin", "admin@javaops.ru", UserFlag.superuser, SPB.getRef());
+        DELETED = new User("Deleted", "deleted@yandex.ru", UserFlag.deleted, SPB.getRef());
+        FULL_NAME = new User("Full Name", "gmail@gmail.com", UserFlag.active, KIEV.getRef());
+        USER1 = new User("User1", "user1@gmail.com", UserFlag.active, MOSCOW.getRef());
+        USER2 = new User("User2", "user2@yandex.ru", UserFlag.active, KIEV.getRef());
+        USER3 = new User("User3", "user3@yandex.ru", UserFlag.active, MINSK.getRef());
         FIST5_USERS = ImmutableList.of(ADMIN, DELETED, FULL_NAME, USER1, USER2);
     }
 
