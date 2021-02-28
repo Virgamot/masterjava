d479cbb9a2d2b51498085d34cd1720a862719be8
ipodskochin <ipodskochin@mail.ru>
2021-02-28 19:34:20 +0300

    HW07(base) mailService.wsdl mv to common resources, wsdl fixed

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailWSClient.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailWSClient.java
@@ -24,4 +24,9 @@ public class MailWSClient {
         log.info("Send mail to '" + to + "' cc '" + cc + "' subject '" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
         WS_CLIENT.getPort().sendToGroup(to, cc, subject, body);
     }
+
+    public static void sendBulk(final Set<Addressee> to, final String subject, final String body) {
+        log.info("Send mail to '" + to + "' subject '" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
+        WS_CLIENT.getPort().sendBulk(to, subject, body);
+    }
 }
--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -1,12 +1,14 @@
 package ru.javaops.masterjava.service.mail;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 
 public class MailWSClientMain {
     public static void main(String[] args) {
+        MailWSClient.sendBulk(
+                ImmutableSet.of(new Addressee("To <ipodskochin@mail.ru>")), "Subject", "BodyBulk");
+
         MailWSClient.sendToGroup(
-                ImmutableSet.of(new Addressee("To <masterjava@javaops.ru>")),
-                ImmutableSet.of(new Addressee("Copy <masterjava@javaops.ru>")), "Subject", "Body");
+                ImmutableSet.of(new Addressee("To <ipodskochin@mail.ru>")),
+                ImmutableSet.of(new Addressee("Copy <ipodskochin@mail.ru>")), "Subject", "BodyGroup");
     }
 }
